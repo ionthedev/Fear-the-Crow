baf475f3a54c36743aaa5a21dfb50969ff6f5275
ionthedev <brandon@friendlystudios.net>
2024-07-24 00:31:08 -0500

    Moved window and FPS ceiling intitialization to engine.start()

--- src/main.cpp
+++ src/main.cpp
@@ -13,37 +13,24 @@
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-
     engine_core engine = engine_core(true);
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-    const double deltaTime = 0;
-
-    InitWindow(screenWidth, screenHeight, "Phantom Engine - Window");
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    engine.Start();
     //--------------------------------------------------------------------------------------
-
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         engine.Loop();
         BeginDrawing();
         engine.Render();
-        //Middle Process
         EndDrawing();
     }
-
     // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
-
     return 0;
 }
