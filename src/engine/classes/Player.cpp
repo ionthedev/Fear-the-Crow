b6449b3cdcf5b8c87209fece14d282d3967bf400
Brandon <brandon@friendlystudios.net>
2024-07-28 12:51:24 -0500

    Began player rewrite

--- src/engine/classes/Player.cpp
+++ src/engine/classes/Player.cpp
@@ -11,147 +11,29 @@ void Player::Init()
     Husk::Init();
 }
 
-void Player::Start()
-{
-    Husk::Start();
-    GetPlayerCamera();
+void Player::MouseLook(double _deltaTime) {
+
+    Vector2 mDelta = GetMouseDelta();
+    player_data.rotationalVelocity.y += -mDelta.x * settings_sensitivity * player_data.PLAYER_LOOK_SENSITIVITY;
+    player_data.rotationalVelocity.x += -mDelta.y * settings_sensitivity * player_data.PLAYER_LOOK_SENSITIVITY;
+
+    player_data.lookRotationEuler = Vector3Add(player_data.lookRotationEuler, Vector3Scale(player_data.rotationalVelocity, _deltaTime * 4.0f));
+    player_data.lookRotationEuler.x = Clamp(player_data.lookRotationEuler.x, -PI * 0.45f, PI * 0.45f);
+
+    player_data.rotationalVelocity = Vector3Scale(player_data.rotationalVelocity, 0.92f);
+
+    player_data.lookRotationMat3 = MatrixRotateXYZ(player_data.lookRotationEuler);
+    player_data.lookDir = Vector3Transform((Vector3){0,0,1}, player_data.lookRotationMat3);
+}
+
+void Player::CalculateInputs() {
+    Vector3 acceleration = { 0 };
+    acceleration = IsKeyDown(KEY_W) ? Vector3Add(acceleration, player_data.lookDir) :
+              (IsKeyDown(KEY_S) ? Vector3Subtract(acceleration, player_data.lookDir) : acceleration);
+
+    acceleration = IsKeyDown(KEY_A) ? Vector3CrossProduct(player_data.lookDir, (Vector3){0, 1, 0}) :
+              (IsKeyDown(KEY_D) ? Vector3CrossProduct(player_data.lookDir, (Vector3){0, 1, 0}) : acceleration);
 
 }
 
-void Player::Update(double _deltaTime)
-{
-
-    CalculateMovement(_deltaTime);
-}
-
-void Player::FixedUpdate(double _deltaTime)
-{
-
-    HandleInput();
-    Husk::FixedUpdate(_deltaTime);
-
-    //CameraYaw(&camera, GetMouseDelta().x * 0.004f, false);
-
-}
-
-void Player::Render()
-{
-    Husk::Render();
-}
-
-void Player::HandleInput()
-{
-    Husk::HandleInput();
-
-
-}
-
-void Player::SetIsActive(bool _isActive)
-{
-    Husk::SetIsActive(_isActive);
-}
-
-bool Player::GetIsActive()
-{
-    return Husk::GetIsActive();
-}
-
-Camera& Player::GetPlayerCamera()
-{
-    return camera;
-}
-
-void Player::HandleMovement()
-{
-    Husk::HandleMovement();
-
-}
-
-void Player::CalculateMovement(double _deltaTime)
-{
-
-    // Update the velocity based on the wish direction and player speed
-    velocity = Vector3Scale(wishDir(), CURR_SPEED * _deltaTime);
-
-    // Update the position based on the velocity
-    position = Vector3Add(position, velocity);
-
-    // Update the camera position based on the player's position
-    camera.position = position;
-
-    // Optionally update the camera target to maintain a specific view direction
-    camera.target = Vector3Add(camera.position, Vector3Normalize(Vector3Subtract(camera.target, camera.position)));
-}
-
-Vector3 Player::Friction(Vector3 _velocity, float _deltaTime)
-{
-
-    return Vector3Scale(_velocity, GROUND_FRICTION * _deltaTime);
-}
-
-Vector3 Player::wishDir()
-{
-    Vector3 forward = Vector3Normalize(Vector3Subtract(camera.target, camera.position));
-
-    // Right direction is perpendicular to forward and up vector (assuming up vector is (0, 1, 0))
-    Vector3 right = Vector3Normalize(Vector3CrossProduct(forward, { 0, 1, 0 }));
-
-    // Wish direction is a combination of forward and right directions
-    Vector3 wishDir = {
-        forward.x * inputDir().z + right.x * inputDir().x,
-        0, // We don't want any vertical movement here
-        forward.z * inputDir().z + right.z * inputDir().x};
-
-    wishDir = Vector3Normalize(wishDir);
-
-    std::cout << "Vector3(" << wishDir.x << ", " << wishDir.y << ", " << wishDir.z << ")" << std::endl;
-
-    return wishDir;
-
-}
-
-Vector3 Player::CalculateGroundVelocity(Vector3 _wishDir, Vector3 _velocity, float _deltaTime)
-{
-    //_velocity = Friction(_velocity, deltaTime);
-    //float ADD_SPEED = PhanthomEngine::m_Clip(MAX_SPEED, 0, MAX_SPEED * _deltaTime);
-    Vector3 out = Vector3Multiply(_wishDir, Vector3Scale(_velocity, MAX_SPEED * _deltaTime));
-    //std::cout << "Vector3(" << _wishDir.x << ", " << _wishDir.y << ", " << _wishDir.z << ")" << std::endl;
-
-    return _wishDir;
-}
-
-void Player::SetCameraRotation()
-{
-    Vector3 forward = GetCameraForward(&GetPlayerCamera());
-    Vector3 right = GetCameraRight(&GetPlayerCamera());
-
-    rotation.x = asinf(forward.y) * RAD2DEG;
-    rotation.y = atan2f(forward.x, forward.z) * RAD2DEG;
-    rotation.z = atan2f(right.y, right.x) * RAD2DEG;
-
-}
-
-Vector3 Player::inputDir()
-{
-    Vector3 _inputDir = { 0 };
-    _inputDir.z = (IsKeyDown(KEY_W) - IsKeyDown(KEY_S));
-    _inputDir.x = (IsKeyDown(KEY_D) - IsKeyDown(KEY_A));
-
-    if (_inputDir.x != 0 || _inputDir.z != 0) {
-        _inputDir = Vector3Normalize(_inputDir);
-    }
-
-    return _inputDir;
-}
-
-Camera Player::camera = Camera
-{
-    { 0.0f, 1.0f, 2.0f },
-    { 0.0f, 1.0f, 0.0f },
-    { 0.0f, 1.0f, 0.0f },
-    90.0f,
-    CAMERA_PERSPECTIVE
-
-
-};
 
--- src/engine/classes/Player.h
+++ src/engine/classes/Player.h
@@ -7,11 +7,44 @@
 #include "../PhantomEngine.h"
 
 
-
+#define PLAYER_MAX_HEALTH 10
+#define PLAYER_HEAD_CENTER_OFFSET 0.9f
+#define PLAYER_LOOK_SENSITIVITY 0.001f
+#define PLAYER_FOV 110
+#define PLAYER_VIEWMODEL_FOV 90
+#define PLAYER_BOX_SIZE (Vector3){1.2f, 2.2f, 1.2f}
+#define PLAYER_BOX_RAD 0.05f
+#define PLAYER_SIZE (Vector3){PLAYER_BOX_SIZE.x + PLAYER_BOX_RAD, PLAYER_BOX_SIZE.y + PLAYER_BOX_RAD, PLAYER_BOX_SIZE.z + PLAYER_BOX_RAD}
+#define PLAYER_HEAD_SIN_TIME (PI * 5.0f)
+#define PLAYER_GRAVITY 220 // 800
+#define PLAYER_SPEED 100 // 320
+#define PLAYER_GROUND_ACCELERATION 8 // 10
+#define PLAYER_GROUND_FRICTION 1 // 6
+#define PLAYER_AIR_ACCELERATION 0.6f // 0.7
+#define PLAYER_AIR_FRICTION 0.0f // 0
+#define PLAYER_JUMP_SPEED 60 // 270
+#define PLAYER_MIN_NORMAL_Y 0.25f
+#define PLAYER_FALL_DEATH_Y (-TILE_HEIGHT * 1.8f)
+#define PLAYER_FALL_DEATH_START_DARKEN (PLAYER_FALL_DEATH_Y * 0.3f)
+#define PLAYER_KEY_JUMP KEY_SPACE
 
 class Player : public virtual Husk {
 
 public:
+
+
+    struct PlayerData {
+        Vector3 position;
+        Vector3 lookRotationEuler;
+        Matrix lookRotationMat3;
+        Vector3 lookDir;
+        Vector3 rotationalVelocity; //used for calculating rotations from mouse input.
+        bool bIsGrounded;
+        Vector3 velocity;
+        Vector3 lastPosition;
+        Vector3 lastVelocity;
+        Vector3 inputDir;
+    } player_data;
 
 
     void Init() override;
@@ -22,30 +55,13 @@ public:
     auto HandleInput() -> void override;
     void SetIsActive(bool _isActive) override;
     bool GetIsActive() override;
-    static Camera& GetPlayerCamera();
+    float settings_sensitivity = 0.04f;
 
     static Camera camera;
 
 protected:
-    void HandleMovement() override;
-    void CalculateMovement(double _deltaTime);
-    float deltaTime;
-    float MAX_SPEED = 3.0f;
-    float CURR_SPEED = 5.0f;
-    float MAX_ACCEL = 10.0f * MAX_SPEED;
-    float GROUND_FRICTION = 6.0f;
-
-    Vector3 Friction(Vector3 _velocity, float _deltaTime);
-    Vector3 wishDir();
-
-    Vector3 CalculateGroundVelocity(Vector3 _wishDir, Vector3 _velocity, float _deltaTime);
-    void SetCameraRotation();
-
-    Vector3 inputDir();
-    Vector3 velocity;
-
-    Vector3 position = { 0, 1, 0};
-    Vector3 rotation;
+    void MouseLook(double _deltaTime);
+    void CalculateInputs();
 
 };
 
