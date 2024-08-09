68d97374e08b8f8c93dec8c1cf2364845dc67a02
ionthedev <brandon@friendlystudios.net>
2024-08-08 23:58:27 -0500

    This one is a doozy - Created Engine Entry Point - Restructured Engine to be actually reusable - Fixed Linker again.

--- src/FearTheCrow.cpp
+++ src/FearTheCrow.cpp
@@ -2,12 +2,27 @@
 // Created by Brandon Friend on 8/7/24.
 //
 
+#include <iostream>
+
 #include "PhantomEngine.h"
+#include "Core/EntryPoint.h"
 
-int main()
+class FearTheCrow : public Phantom::Application
 {
+public:
+    FearTheCrow()
+    {
 
-    return 0;
+    }
+
+    ~FearTheCrow()
+    {
+
+    }
+};
+
+Phantom::Application* Phantom::CreateApplication()
+{
+    return new FearTheCrow();
 }
-
 
